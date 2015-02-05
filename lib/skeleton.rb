9b7baf7ea99eaf1c76525424a45b3f7289b0fcfb
Matthew Johnston <warmwaffles@gmail.com>
2015-02-05 09:48:34 -0600

    move require to top of file

--- lib/skeleton.rb
+++ lib/skeleton.rb
@@ -1,7 +1,12 @@
 require 'skeleton/version'
 require 'skeleton/structure'
+require 'skeleton/serializers/swagger'
+require 'skeleton/serializers/options'
 
 module Skeleton
+  # Allows you to build a skeleton structure from within a block
+  #
+  # @return [Skeleton::Structure]
   def self.build(&block)
     structure = Skeleton::Structure.new
     yield(structure) if block
@@ -9,5 +14,3 @@ module Skeleton
   end
 end
 
-require 'skeleton/serializers/swagger'
-require 'skeleton/serializers/options'
